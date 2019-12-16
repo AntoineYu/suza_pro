30b3606d431ffa330064a4ac98889973d9de3ec2
AntoineYu <yzq356350@gmail.com>
2019-12-16 13:59:18 +0100

    configuration de router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Acceuil = () => import('views/acceuil/Acceuil')
+const Categorie = () => import('views/categorie/Categorie')
+const Panier = () => import('views/panier/Panier')
+const Compte = () => import('views/compte/Compte')
+
 Vue.use(VueRouter)
 
 const routes = [
-  
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/accueil',
+    component: Acceuil
+  },
+  {
+    path: '/categorie',
+    component: Categorie
+  },
+  {
+    path: '/panier',
+    component: Panier
+  },
+  {
+    path: '/compte',
+    component: Compte
+  }
 ]
 
 const router = new VueRouter({
