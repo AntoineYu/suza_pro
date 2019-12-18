32d7d0649332e265ded64c5586889c31f3e26ebc
AntoineYu <yzq356350@gmail.com>
2019-12-18 17:20:18 +0100

    installation de better-scroll

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-
+  <div ref="wrapper">
+      <div class="content">
+          <slot></slot>
+      </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
     name: 'Scroll',
 }
