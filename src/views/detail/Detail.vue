25b97b0318ed1f20ca46c89a71aef2fac42b59a3
AntoineYu <yzq356350@gmail.com>
2019-12-20 10:46:18 +0100

    navbar fini pour le page detail

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
-      <p>{{id}}</p>
+      <detail-nav-bar></detail-nav-bar>
   </div>
 </template>
 <script>
+import DetailNavBar from './childComps/DetailNavBar'
 export default {
     name: 'Detail',
+    components: {
+        DetailNavBar
+    },
     data() {
         return {
-            id: null
+        
         }
     },
-    created() {
-        console.log(this.$route.params.id);
-        this.id = this.$route.params.id
-    }
+    // created() {
+    //     console.log(this.$route.params.id);
+    //     this.id = this.$route.params.id
+    // }
 }
 </script>
 <style scoped>
-
+    
 </style>
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav-bar class="nav-bar">
+    <div slot="left"><img src="~assets/images/common/back.svg" alt=""
+    @click="backClick">
+    </div>
+    <div slot="center" class="title">
+        <div v-for="(item, index) in titles"
+        class="title-item"
+        :class="{active: currentIndex === index}"
+        @click="titleClick(index)">{{ item }}
+        </div>
+    </div>
+  </nav-bar>
+</template>
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+export default {
+    name: 'DetailNavBar',
+    components: {
+        NavBar
+    },
+    data() {
+        return {
+            titles: ['Article', 'Paramètre', 'Commentaire'],
+            currentIndex: 0
+        }
+    },
+    methods: {
+        titleClick(index) {
+            this.currentIndex = index
+        },
+        backClick() {
+            this.$router.go(-1)
+        }
+    }
+}
+</script>
+<style scoped>
+    .nav-bar {
+    }
+
+    .nav-bar img {
+        vertical-align: middle;
+    }
+
+    .title {
+        display: flex;
+    }
+    
+    .title-item {
+        flex: 1;
+        font-size: 12px;
+        padding: 0 10px;
+    }
+
+    .active {
+        color: var(--color-high-text);
+    }
+</style>
