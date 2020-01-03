8f8cbbb250486c362ee680f06b7b4a680b7d4ef2
AntoineYu <yzq356350@gmail.com>
2020-01-03 15:57:10 +0100

    pas de quoi

--- src/views/categorie/Categorie.vue
+++ src/views/categorie/Categorie.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    <p>Categorie</p>
+  <div class="category">
+    <nav-bar class="category-nav-bar">
+      <div slot="center">Catégorie</div>
+    </nav-bar>
+    <slide-bar></slide-bar>
   </div>
 </template>
 <script>
+import SlideBar from "./childComps/SlideBar"
+
+import NavBar from "@/components/common/navbar/NavBar"
+
 export default {
     name: 'Categorie',
+    components: {
+      SlideBar,
+      NavBar
+    }
 }
 </script>
 <style scoped>
-
+  .category-nav-bar {
+    font-weight: 600;
+    color: white;
+    background-color: var(--color-tint);
+  }
 </style>
