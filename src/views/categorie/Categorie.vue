28a6b384ba73ec099d2f3d517a40a787cdc37039
AntoineYu <yzq356350@gmail.com>
2020-01-06 22:01:28 +0100

    fini le views categorie.vue

--- src/views/categorie/Categorie.vue
+++ src/views/categorie/Categorie.vue
@@ -4,15 +4,16 @@
       <div slot="center">Catégorie</div>
     </nav-bar>
     <slide-bar :slide-bar-list="categoryList"></slide-bar>
-    <!-- <scroll class="content">
+    <scroll class="content" :pullUpLoad="true" @pullingUp="loadMore" ref="scroll">
       <goods-list :cgoods="showGoods"></goods-list>
-    </scroll> -->
+    </scroll>
   </div>
 </template>
 <script>
 import SlideBar from "./childComps/SlideBar"
 
 import NavBar from "@/components/common/navbar/NavBar"
+import Scroll from "@/components/common/scroll/Scroll"
 import GoodsList from "@/components/content/goods/GoodsList"
 
 import {getCategory, getProByCat} from "@/network/categorie"
@@ -22,12 +23,14 @@ export default {
     components: {
       SlideBar,
       NavBar,
+      Scroll,
       GoodsList
     },
     data() {
       return {
         categoryList: [],
-        goods: {'page': 0, 'list': []}
+        goods: {'page': 0, 'list': []},
+        currentItem: ''
       }
     },
     computed: {
@@ -54,13 +57,18 @@ export default {
         getProByCat(category, page).then(res => {
           this.goods.list.push(...res)
           this.goods.page += 1
+          this.$refs.scroll.finishPullUp()
         })
+      },
+      loadMore() {
+        this.updateGoodsData(this.currentItem)
       }
     },
     created() {
       this.getCategory()
       this.$bus.$on('getGoods', value => {
         this.getGoodsData(value)
+        this.currentItem = value
       })
     }
 }
@@ -70,5 +78,14 @@ export default {
     font-weight: 600;
     color: white;
     background-color: var(--color-tint);
+  }
+
+  .content {
+    overflow: hidden;
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 100px;
+    right: 0;
   }
 </style>
