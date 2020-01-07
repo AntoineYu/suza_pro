b87a7cc239ea666706ccd379708db474b89b7b40
AntoineYu <yzq356350@gmail.com>
2020-01-07 15:26:09 +0100

    ajouter les images

--- src/views/compte/Compte.vue
+++ src/views/compte/Compte.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-
+  <div class="profile">
+    <nav-bar class="nav-bar">
+      <div slot="center">Votre compte</div>
+    </nav-bar>
+    <!-- <scroll class="content">
+      
+    </scroll> -->
   </div>
 </template>
 <script>
+import NavBar from "components/common/navbar/NavBar"
+import Scroll from "@/components/common/scroll/Scroll"
 export default {
     name: 'Compte',
+    components: {
+      NavBar,
+      Scroll
+    }
 }
 </script>
 <style scoped>
+  .profile {
+    width: 100%;
+  }
 
+  .nav-bar {
+    font-weight: 600;
+    color: white;
+    background-color: var(--color-tint)
+  }
 </style>
